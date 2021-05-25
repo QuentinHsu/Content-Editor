3a3d32dbd500c987faf53a24969ffce3712d4d59
QuentinHsu <xuquentinyang@gmail.com>
2021-05-25 18:18:16 +0800

    init(global): init project

---
+++ src/views/contentEditor/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="content-editor">
+    <h2>ContentEditor</h2>
+    <contenteditable></contenteditable>
+  </div>
+</template>
+
+<script>
+import Contenteditable from "./components/contenteditable";
+export default {
+  components: { Contenteditable },
+};
+</script>
+
+<style></style>
